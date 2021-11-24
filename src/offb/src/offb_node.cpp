45dbb602e06c9771f637deba988bb4b78d6a1371
Mihir Laud <mihirlaud@hotmail.com>
2021-11-23 22:19:13 -0500

    Added D control

--- src/offb/src/offb_node.cpp
+++ src/offb/src/offb_node.cpp
@@ -13,7 +13,7 @@
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/ManualControl.h>
 
-double x_kP = 1;
+double x_kP = 2;
 double x_kD = 5;
 
 double y_kP = 1;
@@ -22,7 +22,11 @@ double y_kD = 5;
 double z_kP = 1;
 double z_kD = 5;
 
-std::array<double, 3> setpoint = {4, 5, 10};
+double x_prev_error = 0;
+double y_prev_error = 0;
+double z_prev_error = 0;
+
+std::array<double, 3> setpoint;
 
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
@@ -53,15 +57,25 @@ std::array<double, 3> pidControl() {
     double y_error = setpoint[1] - pos.y;
     double z_error = setpoint[2] - pos.z;
 
-    double x_command = limitCommand(x_kP * x_error, 3.0);
-    double y_command = limitCommand(y_kP * y_error, 3.0);
-    double z_command = limitCommand(z_kP * z_error, 3.0);
+    double x_command = limitCommand(x_kP * x_error + x_kD * (x_error - x_prev_error), 3.0);
+    double y_command = limitCommand(y_kP * y_error + y_kD * (y_error - y_prev_error), 3.0);
+    double z_command = limitCommand(z_kP * z_error + z_kD * (z_error - z_prev_error), 3.0);
+
+    x_prev_error = x_error;
+    y_prev_error = y_error;
+    z_prev_error = z_error;
 
     return {x_command, y_command, z_command};
 }
 
 int main(int argc, char **argv)
 {
+    if(argc == 4) {
+        setpoint = {std::stod(argv[1]), std::stod(argv[2]), std::stod(argv[3])};
+    } else {
+        setpoint = {0, 0, 1};
+    }
+
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
@@ -114,19 +128,16 @@ int main(int argc, char **argv)
     cmd_vel.angular.z = 0;
 
     while(ros::ok()){
-            if( current_state.mode != mavros_msgs::State::MODE_PX4_OFFBOARD &&
-				(ros::Time::now() - last_request > ros::Duration(5.0))){
-				if( set_mode_client.call(offb_set_mode) &&
-						offb_set_mode.response.mode_sent){
-						ROS_INFO("Offboard enabled");
-				}
-				last_request = ros::Time::now();
-            }
+        if( current_state.mode != mavros_msgs::State::MODE_PX4_OFFBOARD && (ros::Time::now() - last_request > ros::Duration(5.0))){
+			if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent){
+				ROS_INFO("Offboard enabled");
+			}
+			last_request = ros::Time::now();
+        }
 
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
+        if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( arming_client.call(arm_cmd) &&
+                arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
@@ -134,8 +145,14 @@ int main(int argc, char **argv)
 
         std::array<double, 3> command = pidControl();
 
-        cmd_vel.linear.x = command[0];
-        cmd_vel.linear.y = command[1];
+        if(current_pose.pose.position.z > 0.75) {
+            cmd_vel.linear.x = command[0];
+            cmd_vel.linear.y = command[1];
+        } else {
+            cmd_vel.linear.x = 0;
+            cmd_vel.linear.y = 0;
+        }
+
         cmd_vel.linear.z = command[2];
 
         manual_control_pub.publish(control);
